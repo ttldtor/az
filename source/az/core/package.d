929c2a9a40a4c838ab495d724cbdb1401a1133ff
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-07 20:45:13 +0300

    SortOrder -> package

--- source/az/core/package.d
+++ source/az/core/package.d
@@ -3,6 +3,7 @@ module az.core;
 public import az.core.az;
 public import az.core.color;
 public import az.core.role;
+public import az.core.sortorder;
 
 public import az.core.model;
 public import az.core.models;
